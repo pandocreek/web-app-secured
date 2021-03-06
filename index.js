b30690083d3d206e0773017b0b1be0274bf6ec7b
Nacho <esmite@icloud.com>
2021-03-06 17:25:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,10 @@ app.use(
     authRequired: false,
     auth0Logout: true,
     baseURL: APP_URL,
+    authorizationParams: {
+     response_type: "code id_token",
+     audience: "https://expenses-api",
+   },
   })
 );
 
@@ -71,7 +75,14 @@ app.get("/user", requiresAuth(), async (req, res) => {
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
   try {
-    const expenses = await axios.get(`${API_URL}/reports`);
+     // 👇 get the token from the request 👇
+     const { token_type, access_token } = req.oidc.accessToken;
+     // 👇 then send it as an authorization header 👇
+     const expenses = await axios.get(`${API_URL}/reports`, {
+       headers: {
+         Authorization: `${token_type} ${access_token}`,
+       },
+     });
     res.render("expenses", {
       user: req.oidc && req.oidc.user,
       expenses: expenses.data,
